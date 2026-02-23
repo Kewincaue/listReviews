9b68e2b5fa2c0fba028378b64668cb0db6dede06
kewin3 <kewin.caue@hotmail.com>
2026-02-23 20:20:09 -0300

    PrimeirosPassosPython

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
-print ("Olá Mundo!");
+name = input('Digite seu nome: ');
+idade = input('Digite sua idade: ');
+cidade= input('Digite a cidade que você mora: ');
+
+print ('Seja muito bem vindo {}!'.format(name));
+print ('Fico Feliz em saber que você é de {}, e tem {} anos!'.format(cidade, idade));
